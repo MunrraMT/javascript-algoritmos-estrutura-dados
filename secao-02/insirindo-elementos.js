c3c9111f7f6aeb349fb51bda63ece9815fbf467b
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-03-20 13:35:34 -0400

    6. Removendo elementos

--- secao-02/insirindo-elementos.js
+++ secao-02/insirindo-elementos.js
@@ -1,7 +1,11 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+console.log(numbers);
+
 numbers[numbers.length] = 10;
 numbers[numbers.length] = 11;
+
+console.log(numbers);
 
 numbers.push(12);
 numbers.push(13);
@@ -9,9 +13,25 @@ numbers.push(14);
 numbers.push(15);
 numbers.push(16);
 
+console.log(numbers);
+
 numbers.unshift(-1);
 numbers.unshift(-2);
 numbers.unshift(-3);
 numbers.unshift(-4);
+
+console.log(numbers);
+
+numbers.pop();
+numbers.pop();
+numbers.pop();
+numbers.pop();
+
+console.log(numbers);
+
+numbers.shift();
+numbers.shift();
+numbers.shift();
+numbers.shift();
 
 console.log(numbers);
